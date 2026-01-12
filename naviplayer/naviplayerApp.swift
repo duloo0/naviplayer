1a127a27d72ed29fde66d1b80e80815382cccd18
duloo0 <98614962+duloo0@users.noreply.github.com>
2026-01-12 11:14:38 -0500

    Initial Commit

---
+++ naviplayer/naviplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  naviplayerApp.swift
+//  naviplayer
+//
+//  Created by Caleb McClure on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct naviplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ naviplayerTests/naviplayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  naviplayerTests.swift
+//  naviplayerTests
+//
+//  Created by Caleb McClure on 1/12/26.
+//
+
+import Testing
+@testable import naviplayer
+
+struct naviplayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
